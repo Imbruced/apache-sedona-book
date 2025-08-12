398826dcedb2aa5394c426cc8674908a32139597
pawelkocinski <pawel93kocinski@gmail.com>
2025-08-12 23:32:30 +0200

    Add chapter 3 part one with minor changes to docker

---
+++ docker/sedona/apply.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+/opt/start.sh &
+PID=$!
+
+sleep 5
+if [[ "$SEDONA_COPY_MINIO" = "true" ]]; then
+  echo "Copying data from MinIO to local storage..."
+    cp s3-conf.conf /opt/spark/conf/spark-defaults.conf
+else
+  cp s3-conf.conf /opt/spark/conf/spark-defaults.conf
+fi
+
+wait $PID
