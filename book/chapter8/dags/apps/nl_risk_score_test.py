5b7faf414882564b5cf06225b1cd132d1e638b3d
pawelkocinski <pawel93kocinski@gmail.com>
2025-08-30 22:02:36 +0200

    Add chapter 8

--- book/chapter8/dags/apps/nl_risk_score_test.py
+++ book/chapter8/dags/apps/nl_risk_score_test.py
@@ -14,13 +14,14 @@ sedona = s.SedonaContext.create(config)
 
 risk_score = sedona.table(f"{CATALOG_NAME}.{input_database}.nl_road_risk_score").\
     selectExpr("ST_GeomFromEWKB(geometry) AS geometry", "RISK_SCORE").\
-    filter("RISK_SCORE IS NOT NULL")
+    filter("RISK_SCORE <> 0")
 
 risk_score_count = risk_score.count()
 
-assert risk_score_count > 100, "Quality check failed: Risk score count is greater than 100"
-assert risk_score_count < 200, "Quality check failed: Risk score count is less than 200"
+assert risk_score_count > 3800, "Quality check failed: Risk score count is greater than 3800"
+assert risk_score_count < 3900, "Quality check failed: Risk score count is less than 3900"
 assert risk_score.selectExpr("ST_Area(geometry) AS area")\
             .filter("area <= 0")\
             .count() == 0, "Quality check failed: Invalid area"
-assert risk_score.filter("RISK_SCORE > 1.5").count() == 0, "Quality check failed: Invalid risk score"
+
+assert risk_score.filter("RISK_SCORE > 1.0").count() == 0, "Quality check failed: Invalid risk score"
